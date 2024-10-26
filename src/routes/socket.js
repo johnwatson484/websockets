7e4fb311cc392a25e88152f606a3e22eef6c5df0
John Watson <john.watson1@defra.gov.uk>
2024-10-26 23:34:08 +0100

    Handle request response

--- src/routes/socket.js
+++ src/routes/socket.js
@@ -1,11 +1,17 @@
+import crypto from 'crypto'
+
 const route = {
   method: 'POST',
   path: '/socket',
   config: {
-    plugins: { websocket: { only: true, autoping: 30 * 1000 } }
+    plugins: {
+      websocket: { only: true, autoping: 30 * 1000 },
+      crumb: false
+    }
   },
   handler: (request, h) => {
-    return { data: 1, seen: request.payload }
+    console.log('Received data:', request.payload)
+    return { data: crypto.randomUUID() }
   }
 }
 
