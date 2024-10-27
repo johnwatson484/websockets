8dfb5be9c84439f232e8b50cd3c1f63b0d6346fc
John Watson <john.watson1@defra.gov.uk>
2024-10-27 18:31:03 +0000

    Add peer sending

--- src/routes/socket.js
+++ src/routes/socket.js
@@ -1,17 +1,25 @@
-import crypto from 'crypto'
-
 const route = {
   method: 'POST',
   path: '/socket',
   config: {
     plugins: {
-      websocket: { only: true, autoping: 120 * 1000 },
+      websocket: { only: true, autoping: 30 * 1000 },
       crumb: false
     }
   },
   handler: (request, h) => {
+    const { ws, peers } = request.websocket()
+
     console.log('Received data:', request.payload)
-    return { message: crypto.randomUUID() }
+    console.log('Connected peers:', peers.length)
+
+    peers.forEach(peer => {
+      peer.send(JSON.stringify({ message: request.payload.message }))
+    })
+
+    ws.send(JSON.stringify({ message: 'You sent a message' }))
+
+    return ''
   }
 }
 
